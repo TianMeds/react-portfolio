84b6e0d0d0bc506cb2e055f796c3cc0660cc58aa
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-08-23 12:35:12 +0800

    Updates

--- src/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Extend the Window interface to include your functions
-interface Window {
-    validateForm?: () => boolean;
-    handleFormSubmit?: (event: Event) => void;
-  }
-  
--- src/scripts/ScrollAnimation.ts
+++
@@ -1,7 +0,0 @@
-export function runScrollAnimation() {
-    const image = document.querySelector('.image_div') as HTMLElement; // Use type assertion to specify the type
-    image.addEventListener('wheel', (evnt: WheelEvent) => {
-      evnt.preventDefault();
-      image.scrollLeft += evnt.deltaY;
-    });
-}
